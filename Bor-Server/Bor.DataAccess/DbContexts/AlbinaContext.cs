8945e257ecaa6741c3a66d8377cd170a3d0bb725
NEXT_GM <astashinboris56@gmail.com>
2021-11-25 18:32:46 +0300

    push

---
+++ Bor-Server/Bor.DataAccess/DbContexts/AlbinaContext.cs
@@ -0,0 +1,12 @@
+﻿using Bor.DataAccess.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bor.DataAccess.DbContexts
+{
+    public class AlbinaContext : DbContext, IContext
+    {
+        public AlbinaContext(DbContextOptions<AlbinaContext> options) : base(options)
+        {
+        }
+    }
+}
