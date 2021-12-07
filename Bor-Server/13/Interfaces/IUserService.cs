0e24c82f30b9ad1b8f4e3e6c1d98ca7010b31400
NEXT_GM <astashinboris56@gmail.com>
2021-12-07 19:22:17 +0300

    Добавил update

---
+++ Bor-Server/13/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Bor.BusinessLogic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bor.BusinessLogic.Core.Interfaces
+{
+    public interface IUserService 
+    {
+        Task <UserInformationBlo> Register(UserIdentityBlo userIdentityBlo);
+        Task<UserInformationBlo> Auth(int numberprefix, int number, string password);
+        Task<UserInformationBlo> Update(UserUpdateBlo userUpdateBlo, UserIdentityBlo userIdentityBlo);
+        Task<UserInformationBlo> Get(int userId);
+        Task<bool> DoesExist(int numberPrefix, int number);
+    }
+}
